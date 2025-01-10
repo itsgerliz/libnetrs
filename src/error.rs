45c60a595d87c46242d53a0aa69ff4ac69519c94
itsgerliz <gerworks.hs@gmail.com>
2025-01-10 13:35:06 +0100

    error: Added new is_success and is_failure methods

--- src/error.rs
+++ src/error.rs
@@ -7,10 +7,26 @@ pub enum NetOpResult<V> {
 }
 
 impl <V> NetOpResult<V> {
+	// Checks if the enum is on a Success variant ignoring the inner value
+	pub fn is_success(&self) -> bool {
+		match self {
+			NetOpResult::Success(_) => true,
+			NetOpResult::Failure(_) => false
+		}
+	}
+
+	// Checks if the enum is on a Failure variant ignoring the inner value
+	pub fn is_failure(&self) -> bool {
+		match self {
+			NetOpResult::Success(_) => false,
+			NetOpResult::Failure(_) => true
+		}
+	}
+
 	// Checks the enum exposing two different cases:
 	// Success is found -> Consume the variant and return the inner value as reference
 	// Failure is found -> Terminate the process with specified exit code 
-	fn check(&self, exit_code: i32) -> &V {
+	pub fn check(&self, exit_code: i32) -> &V {
 		match self {
 			NetOpResult::Success(value) => value,
 			NetOpResult::Failure(_) => exit(exit_code),
